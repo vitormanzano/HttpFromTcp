1f8fea92a1d6604e0f90733a125e748ccc4d819d
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-31 22:25:17 -0300

    feat: Chapter 1 completed

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+
+import  (
+	"fmt"
+	"log"
+	"os"
+	"bytes"
+	"io"
+)
+
+func getLinesChannel(f io.ReadCloser) <- chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break;
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i + 1:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+		if len(str) != 0 {
+			out <- str
+		}
+
+	}()
+
+	return out
+}
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("message.txt")
+	if err != nil {
+		log.Fatal("error", "error", err)
+	}
+
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
 }
